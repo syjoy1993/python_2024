cbc017bb8476a19ad74fa82e5b7ca048b5358ad2
syjoy1993 <142670067+syjoy1993@users.noreply.github.com>
2024-01-03 12:50:22 +0900

    Add files via upload

---
+++ day01/퀴즈.py
@@ -0,0 +1,14 @@
+#자기소개
+#이름 입력:
+#mbti 입력:
+#좋아하는 음식 입력:
+# 결과 =>이름이 00이고, mbti가 00이고, 좋아하는 음식이 0이군요
+
+name = input("이름 입력:")
+mbti = input("mbti 입력:")
+food = input("최애 음식:")
+print(f"이름은 {name} mbti눈 {mbti} 최애 음식은 {food}이군요!")
+
+movie = input("최근에 본 영화는?")
+ott = input("최근에 본 ott는?")
+print(f"최근에 본 영화는:{movie} 최근에 본 ott:{ott}")
